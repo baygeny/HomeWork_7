7faaccf9a5afb8d87bd19b4f45b7ed163f277a03
Vadym Baidak <vbaidak@langate.com>
2022-12-13 14:48:35 +0200

    Prod function

---
+++ src/Prod.java
@@ -0,0 +1,7 @@
+public class Prod {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("Hello world!");
+        System.out.println("Hello everybody!");
+    }
+}
